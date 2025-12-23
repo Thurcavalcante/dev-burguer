13ceff40de6686bae8e7f7f9d4c7c8d921941588
Arthur Cavalcante <arthurcavalcantethur@gmail.com>
2025-12-23 13:43:38 -0300

    Cardapio: Exibi imagem de capa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-MediaSourceHandle.exports = {
-  content: ["./**/*.{html,js}"],
+module.exports = {
+  content: ["./src/**/*.{html,css,js}", "./public/**/*.{html,css,js}"],
   theme: {
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
@@ -13,19 +13,3 @@ MediaSourceHandle.exports = {
   },
   plugins: [],
 };
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./**/*.{html,js}"],
-//   theme: {
-//     fontFamily:{
-//       'sans': ['Poppins', 'sans-serif']
-//     },
-//     extend: {
-//       backgroundImage:{
-//         "home": "url('/assets/bg.png')"
-//       }
-//     },
-//   },
-//   plugins: [],
-// }
